2b3da6361dbf4e4a69894a9ea8434f0c1b1f6b97
FraolCode <fraol.code@gmail.com>
2022-06-02 15:48:40 -0700

    ch

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
 /**
  *  * main - Entry point
  *   *
- *    * Return
+ *    * Return: Always 0 (Success)
  *     */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
