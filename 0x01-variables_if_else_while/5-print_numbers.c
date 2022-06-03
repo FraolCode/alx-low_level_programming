60992ab78e3f442b3e1122f1043271f32ed2c3d6
FraolCode <fraol.code@gmail.com>
2022-06-03 10:00:44 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers from 0 to 9
+ * base 10
+ * Return: return 0 if it is success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; n++)
+	{
+		printf("%d", num);
+	}
+	printf('\n');
+	return (0);
+}
