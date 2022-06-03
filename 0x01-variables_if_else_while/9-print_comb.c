64ee9ff6c7d92e973300135d20db0deaed16db90
FraolCode <fraol.code@gmail.com>
2022-06-03 12:13:42 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single-digit numbers
+ * Return: return if it succuss
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num =! 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putch('\n');
+	return (0);
+}
