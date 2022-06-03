51dfcc75e5b33764418e9050f544d03b5ea59b05
FraolCode <fraol.code@gmail.com>
2022-06-03 12:05:20 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print all hexadecimal numbers
+ * in lowercase
+ * followed by new line
+ * Return: return 0 if it is successful
+ */
+int main(void)
+{
+	int num;
+	char alp;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	for (alp = 'a'; ch <= 'f'; c++)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
