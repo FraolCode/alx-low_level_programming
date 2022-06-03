cf0c5d22af3dc0e2697f0cb1e6f86f801d508f6f
FraolCode <fraol.code@gmail.com>
2022-06-03 11:48:54 -0700

    reversed alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main print alphabet in reverse
+ * folowed by a new line
+ * Return: return 0 if it is success
+ */
+int main(void)
+{
+	char alp;
+	for (alp = 'z'; alp >= 'a'; alp--)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
