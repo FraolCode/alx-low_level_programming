3484b0c4bfd00201d92b9399b05d2bbca62779fc
FraolCode <fraol.code@gmail.com>
2022-06-03 12:50:05 -0700

    different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,16 +6,20 @@
  */
 int main(void)
 {
-	int fnum,snum;
+	int fnum, snum;
 
 	for (fnum = 48; fnum < 58; fnum++)
 	{
 		for (snum = 48; snum < 58; snum++)
 		{
-			if(fnum != snum && fnum < snum)
+			if (fnum != snum && fnum < snum)
 			{
 				putchar(fnum);
 				putchar(snum);
+				if (fnum == 56)
+				{
+					break;
+				}
 				putchar(',');
 				putchar(' ');
 			}
