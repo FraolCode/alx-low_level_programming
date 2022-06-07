37b607afdc6e73579a3ab795ad04c621d7063043
Fraol Girma <101295167+FraolCode@users.noreply.github.com>
2022-06-07 12:42:17 -0800

    prints numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,31 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@FraolCode 
-calebapril
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-alx-low_level_programming/0x02-functions_nested_loops/11-print_to_98.c
-@calebapril
-calebapril 11 commit
-Latest commit 9500e78 on 15 Mar
- History
- 1 contributor
-Executable File  32 lines (30 sloc)  383 Bytes
-   
 #include <stdio.h>
 #include "main.h"
 
