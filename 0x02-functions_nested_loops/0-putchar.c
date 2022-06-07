dd5026a7f0b3f32b91872d55a547862f1229ae24
FraolCode <fraol.code@gmail.com>
2022-06-07 05:59:01 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
+
 /**
- * main - prints out _putchat
- * Return: return 0(success)
+ * main - Entry point
+ * 
+ * Return: nothing, return void
  */
+
 int main(void)
 {
-	printf("_putchar");
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
+
